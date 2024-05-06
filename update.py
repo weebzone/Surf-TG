a7f7167106c3458eb8711d26a56271bb9e4fd6a6
babupakkakivellu <149465835+babupakkakivellu@users.noreply.github.com>
2024-05-06 16:18:49 +0530

    Update update.py

--- update.py
+++ update.py
@@ -15,7 +15,7 @@ basicConfig(
 )
 load_dotenv('config.env', override=True)
 
-UPSTREAM_REPO = getenv('UPSTREAM_REPO', "https://github.com/weebzone/Surf-TG")
+UPSTREAM_REPO = getenv('UPSTREAM_REPO', "https://github.com/babupakkakivellu/Surf-TG")
 UPSTREAM_BRANCH = getenv('UPSTREAM_BRANCH', "main")
 
 if UPSTREAM_REPO is not None:
