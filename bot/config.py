0f7f2a5ae4db55e6cbdac91bfc50bfec88f9e710
HYBRID <48980248+hybridvamp@users.noreply.github.com>
2024-11-28 09:42:47 +0530

    make use of env vars

--- bot/config.py
+++ bot/config.py
@@ -1,8 +1,9 @@
 from os import getenv
 from dotenv import load_dotenv
+from pathlib import Path
 
-load_dotenv("config.env")
-
+if Path("config.env").exists():
+    load_dotenv("config.env")
 
 class Telegram:
     API_ID = int(getenv("API_ID", "0"))
@@ -10,9 +11,9 @@ class Telegram:
     BOT_TOKEN = getenv("BOT_TOKEN", "")
     PORT = int(getenv("PORT", 8080))
     SESSION_STRING = getenv("SESSION_STRING", "")
-    BASE_URL = getenv("BASE_URL").rstrip('/')
-    DATABASE_URL = getenv("DATABASE_URL")
-    AUTH_CHANNEL = [channel.strip() for channel in getenv("AUTH_CHANNEL").split(",")]
+    BASE_URL = getenv("BASE_URL", "").rstrip('/')
+    DATABASE_URL = getenv("DATABASE_URL", "")
+    AUTH_CHANNEL = [channel.strip() for channel in getenv("AUTH_CHANNEL", "").split(",") if channel.strip()]
     THEME = getenv("THEME", "quartz").lower()
     USERNAME = getenv("USERNAME", "admin")
     PASSWORD = getenv("PASSWORD", "admin")
@@ -20,5 +21,5 @@ class Telegram:
     ADMIN_PASSWORD = getenv("ADMIN_PASSWORD", "surfTG")
     SLEEP_THRESHOLD = int(getenv('SLEEP_THRESHOLD', '60'))
     WORKERS = int(getenv('WORKERS', '10'))
-    MULTI_CLIENT = bool(getenv('MULTI_CLIENT', 'False'))
-    HIDE_CHANNEL = bool(getenv('HIDE_CHANNEL', 'False'))
+    MULTI_CLIENT = getenv('MULTI_CLIENT', 'False')
+    HIDE_CHANNEL = getenv('HIDE_CHANNEL', 'False')
